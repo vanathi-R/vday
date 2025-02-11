b65e7b068567c84f8cdd9832304ca376c4cc4f05
vanathi-R <vanathi.r14@gmail.com>
2025-02-11 19:59:13 +0530

    Add files via upload

---
+++ script - Copy.js
@@ -0,0 +1,7 @@
+document.getElementById("yesButton").addEventListener("click", function() {
+    document.getElementById("responseSection").innerHTML = "<p>Yay! I’m so happy! 💖 Let’s make this official! 😘</p>";
+});
+
+document.getElementById("noButton").addEventListener("click", function() {
+    document.getElementById("responseSection").innerHTML = "<p>Oh no... 😢 Maybe someday... <3</p>";
+});
